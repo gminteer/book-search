a29342f68c39821a7905561dfef4075d9adaccdf
gminteer <gminteer@gmail.com>
2020-10-21 18:54:13 -0400

    can login

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,34 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        savedBooks {
+          bookId
+          authors
+          title
+          description
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+    }
+  }
+`;
