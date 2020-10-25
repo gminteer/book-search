0f1b14fd72ed5c8a587327a04e5972c5fef2f3ca
gminteer <gminteer@gmail.com>
2020-10-25 19:54:40 -0400

    forgot to remove require statement for (now) nonexistant routes module

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 
 const db = require('./config/connection');
-const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
